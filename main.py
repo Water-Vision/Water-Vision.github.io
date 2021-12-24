4b4eec4b71942070bc5006b44919196f0bdaedfc
yiğit emiray <yigit.dinc@ug.bilkent.edu.tr>
2021-12-24 18:15:18 +0300

    main.py created

---
+++ main.py
@@ -0,0 +1,6 @@
+import waterdetect
+
+water_mask = wd.DWWaterDetect.run_single(image_folder='D:\Images\Input\Input_Tiete/SENTINEL2A_20190326-132829-354_L2A_T22KGA_C_V2-0',
+                                         temp_folder='D:/temp',
+                                         shape_file='D:/Shp/Area_Tiete.shp'
+                                        )
