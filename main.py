b0a5217cb4dedcc3aa24ff8eeea3e2978826cb53
yiğit emiray <yigit.dinc@ug.bilkent.edu.tr>
2021-12-24 18:21:42 +0300

    main.py created

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import waterdetect as wd
 water_mask = wd.DWWaterDetect.run_single(image_folder='D:\Images\Input\Input_Tiete/SENTINEL2A_20190326-132829-354_L2A_T22KGA_C_V2-0',
                                          temp_folder='D:/temp',
                                          shape_file='D:/Shp/Area_Tiete.shp'
-                                        )
+                                    )
