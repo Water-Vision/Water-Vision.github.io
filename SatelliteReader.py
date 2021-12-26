16f6731609826bb0920329b844d57ce1e60195e1
yiyiay <64712118+yiyiay@users.noreply.github.com>
2021-12-26 15:22:11 +0300

    Thresholding and science

--- SatelliteReader.py
+++ SatelliteReader.py
@@ -45,13 +45,20 @@ class SatelliteReader(ReaderAbstract):
 
     def filteringComparison2(filepath):
         img = cv2.imread(filepath, 0)
+
         # global thresholding
         ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
         # Otsu's thresholding
         ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
         # Otsu's thresholding after Gaussian filtering
         blur = cv2.GaussianBlur(img, (5, 5), 0)
         ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        cv2.imwrite('newHouse.tif', th3)
+        cv2.imshow('otsu', th3)
+
         # plot all the images and their histograms
         images = [img, 0, th1,
                   img, 0, th2,
